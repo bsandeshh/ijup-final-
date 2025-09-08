d431d62b38b624da0c16657d0c93220e85ca1b97
bsandeshh <sandeshbellale2@gmail.com>
2025-09-09 00:58:44 +0530

    database changed

--- database_schema.sql
+++ database_schema.sql
@@ -1,3 +1,12 @@
 -- WARNING: This schema is for context only and is not meant to be run.
 -- Table order and constraints may not be valid for execution.
 
+CREATE TABLE public.contact_messages (
+  id uuid NOT NULL DEFAULT uuid_generate_v4(),
+  name text NOT NULL,
+  email text NOT NULL,
+  subject text NOT NULL,
+  message text NOT NULL,
+  created_at timestamp with time zone NOT NULL DEFAULT now(),
+  CONSTRAINT contact_messages_pkey PRIMARY KEY (id)
+);
